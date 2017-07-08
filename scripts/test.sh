929f292130047221c8930a5b725124a1d9c985d4
Zhen Zhang <izgzhen@gmail.com>
2017-07-08 15:12:02 +0800

    Add more modes and cleaning up

---
+++ scripts/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export PATH="/opt/ghc/7.10.3/bin:$PATH"
+export PATH="/opt/cabal/1.22/bin:$PATH"
+export PATH="$HOME/.cabal/bin:$PATH"
+
+git clone --depth 1 --recursive git://github.com/ghc/ghc
+cd ghc
+git clone https://github.com/snowleopard/hadrian
+cd hadrian
+./build.sh selftest
+./build.sh -j test --verbose --no-progress --progress-colour=never --progress-info=brief --profile=-
