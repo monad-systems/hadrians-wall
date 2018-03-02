c1e083dbdca3c59263653d23c00a36a01800e70f
Zhen Zhang <zhen@pineapple.exchange>
2018-03-02 10:48:18 +0800

    use SCRIPT_PATH

--- scripts/default.sh
+++ scripts/default.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 export PATH="/opt/ghc/7.10.3/bin:$PATH"
 export PATH="/opt/cabal/1.22/bin:$PATH"
 export PATH="$HOME/.cabal/bin:$PATH"
@@ -9,5 +11,5 @@ export PATH="/usr/local/bin:$PATH"
 ./build.sh -j --verbose --no-progress --progress-colour=never --progress-info=brief --profile=-
 ./build.sh -j --verbose --no-progress --progress-colour=never --progress-info=brief --profile=- --install-destdir=$PWD install
 
-$PWD/usr/local/bin/ghc $HOME/hadrian-www/scripts/Test.hs
-$PWD/usr/local/bin/ghc -dynamic --make $HOME/hadrian-www/scripts/Test.hs
+$PWD/usr/local/bin/ghc $SCRIPT_DIR/Test.hs
+$PWD/usr/local/bin/ghc -dynamic --make $SCRIPT_DIR/Test.hs
