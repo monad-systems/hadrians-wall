25a74ef1c731e188c37c7be1af469450b2a42ab5
Zhen Zhang <izgzhen@gmail.com>
2018-04-20 01:04:59 +0800

    use hadrians-brick as data source

--- server.py
+++ server.py
@@ -11,26 +11,22 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 @app.route("/")
 def index():
-    files = os.listdir(THIS_DIR + "/static/logs")
+    files = os.listdir("hadrians-brick/logs")
     logs = []
     durations = []
 
     for file in files:
-        if ".err" in file:
-            prefix = file[0:-8]
-            segments = prefix.split("%");
-            log = {
-                "datetime": datetime.datetime.strptime(segments[0], "%Y-%m-%dT%H:%M:%S.%f"),
-                "mode": segments[1],
-                "duration": float(segments[2]) / 60.0,
-                "result": segments[3],
-                "prefix": urllib.quote_plus(prefix)
-            }
+        entries = open('hadrians-brick/logs/' + file, 'r').read().split('\n')
+        for entry in entries:
+            try:
+                log = json.loads(entry)
+            except json.decoder.JSONDecodeError:
+                continue
             logs.append(log)
-            if log["result"] == "success" and log["mode"] =="default":
+            if log["exit-code"] == 0 and "mode" in log and log["mode"] == "default":
                 durations.append({
-                    "datetime": segments[0],
-                    "duration": float(segments[2]) / 60.0
+                    "timestamp": log['timestamp'], 
+                    "duration": float(log['duration']) / 60.0
                 })
 
     return render_template("index.html", logs=logs, durations_json=json.dumps(durations))
